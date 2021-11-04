0a2b77052f4f1648c01108b4bef84390737355e9
tmgasek <tomaszgasek28@gmail.com>
2021-11-04 12:30:17 +0000

    merged challenge pages into one project

---
+++ components/ImageGrid.js
@@ -0,0 +1,3 @@
+export default function ImageGrid() {
+  return <div></div>;
+}
---
+++ pages/404.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import styles from '../styles/404.module.css';
+
+export default function NotFound() {
+  return (
+    <>
+      <h4 className={styles.title}>404 Not Found</h4>
+      <div className={styles.container}>
+        <div className={styles.imgDiv}>
+          <Image
+            src="/Scarecrow.png"
+            alt="scarecrow"
+            width={400}
+            height={400}
+          />
+        </div>
+
+        <div className={styles.textDiv}>
+          <h1>I have some bad news for you</h1>
+          <p>
+            The page you are looking for might be removed or is temporarily
+            unavailable
+          </p>
+          <button className="btn-primary" onClick={() => console.log('Hello')}>
+            Return home
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
